c545dca5de26e835993a688ff4682692191a7dff
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2023-11-04 17:30:30 +0530

    feat: setting up the sg group

--- main.tf
+++ main.tf
@@ -27,6 +27,47 @@ module "vpc" {
 
 # SG
 
+module "sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "jenkins-sg"
+  description = ""
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress_cidr_blocks      = ["10.10.0.0/16"]
+  # ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  egress_with_cidr_block = [
+    {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  tags = {
+    Name = "jenkins-sg"
+
+  }
+}
+
 
 
 
